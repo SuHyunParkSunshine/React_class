34fdda9831ceacf0edcff6124b105a881b661de9
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-04-20 13:09:55 +0900

    23.04.20_수업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+//사용자 정의 태그!! 앞 첫 글자 대문자 <App /> 반드시 return이 있는 함수를 만들어야 함, 이때까지 배운 태그들은 소문자로 적어줘야댄다.
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"> {/*html에서 class랑 같은것*/}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
